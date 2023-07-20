784ae3c63bc34429b5aeb880f8c533df2f198361
Gabriel <sernat243@gmail.com>
2023-07-20 16:57:50 -0600

    added 5 day forecast data and added weather icon to weather info div

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,12 @@ const apiKey = '89159feb546b2bfd8414fbf0260c7099'
     const cityInput = document.getElementById('cityInput');
 
     const cityElement = document.getElementById('city');
+    const cityIconEl = document.getElementById('city-icon')
     const temperatureElement = document.getElementById('temperature');
     const windElement = document.getElementById('wind');
     const humidityElement = document.getElementById('humidity');
+
+    const forecastDays = document.querySelectorAll('.forecast > div');
 
     searchButton.addEventListener('click', () => {
         const city = cityInput.value;
@@ -26,18 +29,35 @@ const apiKey = '89159feb546b2bfd8414fbf0260c7099'
             const wind = data.list[0].wind.speed;
             const humidity = data.list[0].main.humidity;
             const date = data.list[0].dt_txt;
+            const cityIcon = data.list[0].weather[0].icon;
+            console.log(cityIcon);
             console.log(data);
 
             const formattedDate = dayjs(date).format('MM/DD/YYYY');
     
             // Update the content of the weather-info div
             cityElement.textContent = cityName + " " + "(" + formattedDate + ")";
+            cityIconEl.src = `https://openweathermap.org/img/w/${cityIcon}.png`;
             temperatureElement.textContent = temperature + ' °C';
             windElement.textContent = wind + ' m/s';
             humidityElement.textContent = humidity;
     
-            // Use the data to update the 5-day forecast divs (if needed)
-            // ...
+             // Display 5-day forecast information
+            for (let i = 1; i <= 5; i++) {
+                const forecast = data.list[i];
+                const forecastDate = dayjs(forecast.dt_txt).format('MM/DD/YYYY');
+                const weatherIcon = forecast.weather[0].icon;
+                const temperature = forecast.main.temp;
+                const wind = forecast.wind.speed;
+                const humidity = forecast.main.humidity;
+    
+                const dayElement = forecastDays[i - 1];
+                dayElement.querySelector('.date').textContent = forecastDate;
+                dayElement.querySelector('.weather-icon').src = `https://openweathermap.org/img/w/${weatherIcon}.png`;
+                dayElement.querySelector('.temperature').textContent = temperature + ' °C';
+                dayElement.querySelector('.wind').textContent = wind + ' m/s';
+                dayElement.querySelector('.humidity').textContent = humidity;
+            }
           })
           .catch(error => {
             console.error('Error fetching data:', error);
